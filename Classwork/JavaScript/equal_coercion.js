3d454346ee9e8050c5d1b943b5f2b3db94ba98b8
krishna1584 <abhi19ay@gmail.com>
2024-09-02 09:44:56 +0530

    Classwork

---
+++ Classwork/JavaScript/equal_coercion.js
@@ -0,0 +1,9 @@
+console.log(); //undefined
+
+console.log(1==1); //true
+console.log(1=='1'); //true
+console.log(1==='1'); //false
+console.log("" == 1); //false
+console.log([] == "");   // trueb  this is considered 0
+
+console.log([] == 0); // true
