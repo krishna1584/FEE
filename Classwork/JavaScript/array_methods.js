5f2ffb83520e77bdf23a4caaa529e95ae08cd96d
krishna1584 <abhi19ay@gmail.com>
2024-09-20 14:35:26 +0530

    Classwork

---
+++ Classwork/JavaScript/array_methods.js
@@ -0,0 +1,42 @@
+// Array methods: map,filter, reduce and sort
+
+// Q: Turn the array variables to uppercase and print a new string
+
+// let arr = ['Ankit', 'Harry', 'Bruh', 'Ketan'];
+
+// let upperCaseArr = arr.map(name => name.toUpperCase());
+// console.log(upperCaseArr);
+
+
+
+
+
+// 1. map:
+// Cube of Number:
+
+// let num = [1,2,3,4,5];
+
+// function cube(num){
+//     return num ** 3;
+// }
+// console.log(num.map(cube));
+
+
+
+
+
+// Q: remove even number:
+
+// const num = [1,2,34,53,24,56];
+
+// const oddNumbers = num.filter(n => n % 2 !== 0);
+// console.log(oddNumbers);
+
+// function isEven (num){
+//     if(num % 2 == 0){
+//         return true
+//     }
+//     return false
+// }
+// const evenNum = num.filter(isEven);
+// console.log(evenNum);
