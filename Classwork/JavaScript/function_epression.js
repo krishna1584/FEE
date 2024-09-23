ff35d6d9da7743bf1a7f44c29c40adabd9fb9db8
krishna1584 <abhi19ay@gmail.com>
2024-09-23 14:37:49 +0530

    Classwrok

---
+++ Classwork/JavaScript/function_epression.js
@@ -0,0 +1,22 @@
+// let a = 17;
+// console.log(a);
+// val();
+// const val = function (num){
+//     return num ** 3;
+// };
+//val();
+
+
+
+
+
+// anonymous function:
+
+// const val = function (a,b){
+//     let c = a + b;
+//     return c;
+// };
+
+// // console.log (sum(5,3));
+// // sum(5,3);
+// console.log(val(5,3));
